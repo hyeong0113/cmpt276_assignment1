6983a8392e7f77e593cf0db091e3a3a4172c344f
Hyung June Kwak <kwakdent95@gmail.com>
2021-05-19 22:05:17 -0700

    server added_2105192205

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 
-var http = require('http');
+var http = require('https');
 var server = http.Server(app);
 
 app.use(express.static('public'));
