638bdde790179e0311017e3aa5b067fd0cb60edc
Hyung June Kwak <kwakdent95@gmail.com>
2021-05-20 01:10:16 -0700

    Procfile  added_2105200110

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const PORT = process.env.PORT || 3000;
+
 const express = require('express');
 const app = express();
-
-var http = require('https');
-var server = http.Server(app);
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
@@ -15,6 +13,8 @@ app.set('views', __dirname + '/views');
 
 app.set('view engine', 'html');
 
-server.listen(PORT, function() {
-    console.log("App is running on port: " + PORT);
+app.get('', (req, res) => {
+    res.render('index');
 });
+
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
