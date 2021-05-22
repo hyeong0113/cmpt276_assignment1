998396c9183ddc14aa341acd9f22dcce05121d12
Hyung June Kwak <kwakdent95@gmail.com>
2021-05-22 08:51:40 -0700

    Comment added_2105220851

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -1,7 +1,9 @@
 const NUM_ACTIVITY = 4;
 const FLOAT_POINT = 3;
 
+// Function for calculating cna returning mean grade with input values
 function meanGrade() {
+    // Bring all necessary input values 
     var myGradeElement = document.getElementsByName("my-grade");
     var baseGradeElement = document.getElementsByName("base-grade");
 
@@ -14,6 +16,8 @@ function meanGrade() {
     var percent_arr = new Array(NUM_ACTIVITY);
 
     var error = false;
+
+    // Calculate mean value and store values
     for (i = 0; i < myGradeElement.length; i++) {
         if ((!myGradeElement[i].value && baseGradeElement[i].value) ||
             (myGradeElement[i].value && !baseGradeElement[i].value)) {
@@ -30,23 +34,27 @@ function meanGrade() {
         }
     }
 
+    // Display error when user do not insert enough information for calculation
     if (error) {
         document.getElementById("result").innerHTML = "You need to insert both grades in each activities";
         return;
     }
 
+    // Display 0 when user do not insert any values
     if (totalGrade === 0) {
-
         report_percentage(percent_arr, j);
         document.getElementById("result").innerHTML = "Mean: 0";
         return;
     }
 
+    // Display the result
     report_percentage(percent_arr, j);
     document.getElementById("result").innerHTML = "Mean: " + (totalGrade / count).toFixed(FLOAT_POINT).toString();
 }
 
+// Function for calculating cna returning weighted grade with input values
 function weightGrade() {
+    // Bring all necessary input values 
     var myGradeElement = document.getElementsByName("my-grade");
     var baseGradeElement = document.getElementsByName("base-grade");
     var weightGradeElement = document.getElementsByName("weight");
@@ -61,6 +69,7 @@ function weightGrade() {
     var meanError = false;
     var weightError = false;
 
+    // Calculate weighted value and store values
     for (i = 0; i < myGradeElement.length; i++) {
         if ((!myGradeElement[i].value && baseGradeElement[i].value) ||
             (myGradeElement[i].value && !baseGradeElement[i].value)) {
@@ -73,7 +82,6 @@ function weightGrade() {
             if (weightGradeElement[i].value) {
                 let percent = (parseInt(myGradeElement[i].value)) / (parseInt(baseGradeElement[i].value));
                 percent_arr[i] = percent;
-                // document.getElementById(i).innerHTML = percent + "%";
                 weightedTotalGrade += ((parseInt(myGradeElement[i].value)) / (parseInt(baseGradeElement[i].value)) * weightGradeElement[i].value);
                 weightTotal += parseInt(weightGradeElement[i].value);
             } else {
@@ -84,27 +92,31 @@ function weightGrade() {
         }
     }
 
+    // Display error when user do not insert enough grades for calculation
     if (meanError) {
         document.getElementById("result").innerHTML = "You need to insert both grades in each activities";
         return;
     }
 
+    // Display error when user do not insert weight value for calculation
     if (weightError) {
         document.getElementById("result").innerHTML = "You need to insert weight value for calculation";
         return;
     }
 
+    // Display 0 when user do not insert any values
     if (weightTotal === 0) {
-
         report_percentage(percent_arr, j);
         document.getElementById("result").innerHTML = "Weighted: 0";
         return;
     }
 
+    // Display the result
     report_percentage(percent_arr, j);
     document.getElementById("result").innerHTML = "Weighted: " + (weightedTotalGrade / weightTotal).toFixed(FLOAT_POINT).toString();
 }
 
+// Helper function for displaying each percent of activity
 function report_percentage(percent_arr, j) {
     for (j = 0; j < NUM_ACTIVITY; j++) {
         if (percent_arr[j]) {
